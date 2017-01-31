7d77c331ed23718cc82530aa1f786aa8b0574ba2
oleks <oleks@oleks.info>
2017-01-31 17:07:49 +0100

    Actually use $lint

--- ci/lint-added.sh
+++ ci/lint-added.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 lint=$(dirname "$0")/lint.sh
 
 for file in $(git diff-index --cached --name-only HEAD | grep '\.hs$'); do
-  lint "$file"
+  $lint "$file"
 done
--- ci/lint-all.sh
+++ ci/lint-all.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 lint=$(dirname "$0")/lint.sh
 
 for file in $(git ls-files | grep '\.hs$'); do
-  lint "$file"
+  $lint "$file"
 done
