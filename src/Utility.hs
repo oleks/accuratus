c34b4fe8b9f9a8703caa034d5d0697fabe38dfa4
oleks <oleks@oleks.info>
2017-01-31 15:10:05 +0100

    A couple other useful utilities

--- src/Utility.hs
+++ src/Utility.hs
@@ -1,6 +1,17 @@
 module Utility where
 
+import Data.Set ( fromList, toList )
+
 fix :: Eq a => (a -> a) -> a -> a
 fix f x =
   let y = f x
   in if x == y then x else fix f y
+
+fixn :: Eq a => Int -> (a -> a) -> a -> a
+fixn 0 _ x = x
+fixn n f x =
+  let y = f x
+  in if x == y then x else fixn (n-1) f y
+
+unique :: (Ord a) => [a] -> [a]
+unique = toList . fromList
